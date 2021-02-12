94f6fd417f3074fbd96dfb4a4078222b00e14338
DanDoesMusic <danielgirgis779@gmail.com>
2021-02-12 14:51:17 +0000

    I did the questions

--- problem1.py
+++ problem1.py
@@ -1,1 +1,21 @@
+####inputs for mesurements#######
 
+base = int(input("What is the length of the base: "))
+side = int(input("What is the length of the second side: "))
+hyp = int(input("What is the length of the hypotenose (longest side) : " ))
+
+#### calculating#########
+
+m1 = base**2
+m2 = side**2
+m3 = hyp**2
+
+###done########
+
+if (m1+m2 == m3) or (m1+m3 == m2) or (m2+m3 == m1):
+    print("A triangle with lengths " + str(base) + ", " +
+    str(side) + ", and " + str(hyp) + " forms a right-angled triangle.")
+
+else:
+    print("A triangle with lengths " + str(base) + ", " +
+    str(side) + ", and " + str(hyp) + " does not form a right-angled triangle.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,9 @@
-
+grades = float(input("what grades did you receve last year: "))
+wages = float(input("what wages did you earn last year: "))
+ 
+if grades >= 80 and wages >= 500:
+    print("You get to go to Europe.")
+elif grades >= 80:
+    print("You get to go to California.")
+else:
+    print("You must stay home ")
